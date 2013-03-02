aa6fcdd84291ff1d5bd0640814fb3ec67aefbe9c
Stefan Kanev <stefan.kanev@gmail.com>
2013-03-02 22:12:27 +0200

    Initialize a leiningen project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lectures
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/lectures/core_test.clj
@@ -0,0 +1,7 @@
+(ns lectures.core-test
+  (:use clojure.test
+        lectures.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
