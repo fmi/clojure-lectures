91904f708e5c537ee3f0a0cfb10ca9c5c979208a
Stefan Kanev <stefan.kanev@gmail.com>
2013-03-26 17:59:57 +0200

    Update the project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lectures "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The lectures for the Clojure course in FMI Sofia"
+  :url "http://fmi.clojure.bg/lectures"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
